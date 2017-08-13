2acacebcabf9783098e0b9837beee1ff1e42ea93
Kirill Luchikhin <kluchikhin@gmail.com>
2017-08-13 04:13:40 +0300

    Adding models for lessons

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from profiles.models import Profile
+from profiles.models import Profile, ProfileGroup
+import string
 
 # Create your models here.
 
@@ -18,11 +19,20 @@ class Lesson(models.Model):
     homework = models.TextField(blank = True, default='')
     date = models.DateField(blank = False, null = False)
 
-    course = models.ForeignKey(Course)
-    teacher = models.ForeignKey(Profile)
+    course = models.ForeignKey(Course, null = True)
+    teacher = models.ForeignKey(Profile, null = True)
+    group = models.ForeignKey(ProfileGroup, null = True)
 
     def __str__(self):
         return self.name
 
     def __unicode__(self):
         return self.name
+
+class StudentLesson(models.Model):
+    student = models.ForeignKey(Profile, blank = False, null = False)
+    lesson = models.ForeignKey(Lesson, blank = False, null = False)
+
+    mark = models.CharField(max_length = 1, blank = True, choices = ((x, x) for x in string.ascii_uppercase[:6]))
+    precense = models.BooleanField(default = True)
+    reason_for_presence = models.CharField(max_length = 255, default = '')
--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Course, Lesson
 
